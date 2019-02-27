32c33df2e3ff66789f774bd51098315222f75190
net-ka <a.k.fedosova@gmail.com>
2019-02-28 01:40:59 +0200

    love-triangle: solution

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  let x = 0;
+  let p = preferences;
+  for (i = 0; i<p.length; i++) {
+    let ic = p[i] - 1;
+    let ia = p[ic] - 1;
+    let ib = p[ia] - 1;
+    if (ib == i & ia != i) {
+      x++;
+    }
+  }
+  return x / 3;
+}
+  
